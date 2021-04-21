0116170301d5fde973a4358f093b998e5f8dca5d
anumi1999 <52823306+anumi1999@users.noreply.github.com>
2021-04-21 18:04:00 +0530

    Create Sort_sol.cpp

---
+++ DCP/HARD/CP4/Sort_sol.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false) ;
+    cin.tie(NULL) ;
+    int n;
+    cin >> n ;
+    int arr[n];
+    for( int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+    sort(arr , arr+n);
+    int ans = -1, index = 0;
+    for( int i = 0 ; i < n ; i++ ){
+        if ( arr[i] <= 0 ){
+            continue;
+        }else{
+            index++;
+            if( arr[i] != index ){
+                ans = index;
+                break;
+            }
+        }
+    }
+    if( ans == -1 ){
+        ans = index+1;
+    }
+    cout << ans;
+    return 0;
+}
