71392d614284e2fab221cd7214fde1fab3ebff0b
anumi1999 <52823306+anumi1999@users.noreply.github.com>
2021-05-06 13:52:23 +0530

    6-may

---
+++ Daily_Prep/hard/decode_string.cpp
@@ -0,0 +1,9 @@
+/* You are given a string s consisting of lowercase alphabet characters, digits, and brackets"(" and ")". s encodes a longer string and is represented as concatenation of n(t), where n is the number of times t is repeated, and t is either a regular string or it's another encoded string recursively.
+
+Return the expanded version of s. Note that t can be the empty string.
+
+Example 1
+Input
+s = "2(ye)0(z)2(2(po)w)"
+Output
+"yeyepopowpopow" */ 
