2fc3e4b948630fc7a93e3d3ad6428a5b3689dd80
anumi1999 <52823306+anumi1999@users.noreply.github.com>
2021-08-20 13:56:44 +0530

    Google

---
+++ DCP/EASY/CP73.cpp
@@ -0,0 +1,18 @@
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr){
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr ;
+            curr = nextTemp; 
+        }
+        return prev;
+    }
